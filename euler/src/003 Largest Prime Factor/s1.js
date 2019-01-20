331d79296495c65a19ce5c1e7f333360ffe56c16
Kevin Smith <kevinsmithwebdev>
2019-01-20 10:44:37 -0600

    save

--- euler/src/003 Largest Prime Factor/s1.js
+++ euler/src/003 Largest Prime Factor/s1.js
@@ -1,20 +1,35 @@
-const isPrime = (n) => {
-  const max = Math.sqrt(n);
-  for (let i = 2, s = max; i <= s; i += 1) {
-    if (n % i === 0) return false;
-  }
-  return n !== 1 && n !== 0;
-};
-
 const largestPrimeFactor = (num) => {
-  const max = Math.floor(num / 2);
-  for (let i = max; i >= 2; i -= 1) {
-    // console.log('checking', i, isPrime(i), num % i === 0);
-    if (isPrime(i) && num % i === 0) {
-      return i;
+  let primeFactor = 2;
+  let targetNum = num;
+  const primesArr = [2];
+
+  const newPrimeFactor = (pf) => {
+    let candidatePrime = pf;
+    while (candidatePrime < num) {
+      let isPrime = true;
+      candidatePrime += 1;
+      for (let p; p < primesArr.length; p += 1) {
+        if (candidatePrime % p === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) {
+        primesArr.push(candidatePrime)
+      }
+      return candidatePrime;
+    }
+    return null;
+  };
+
+  while (primeFactor !== targetNum) {
+    if (targetNum % primeFactor === 0) {
+      targetNum /= primeFactor;
+    } else {
+      primeFactor = newPrimeFactor(primeFactor);
     }
   }
-  return null;
+  return primeFactor;
 };
 
 console.log(largestPrimeFactor(600851475143));
