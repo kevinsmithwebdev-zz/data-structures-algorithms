7129591fe1bdd1846206c98a39be6867b2d239d8
Kevin Smith <kevinsmithwebdev>
2019-01-22 20:47:08 -0600

    save

--- euler/src/005 Smallest Multiple/s1.js
+++ euler/src/005 Smallest Multiple/s1.js
@@ -1,40 +1,19 @@
 const smallestMultiple = (num) => {
-  let primeFactor = 2;
-  let targetNum = num;
-  const primesArr = [2];
+  const indexConvert = (num) => num - 2;
   const factors = [];
 
-  const newPrimeFactor = (pf) => {
-    let candidatePrime = pf;
-    while (candidatePrime < num) {
-      let isPrime = true;
-      candidatePrime += 1;
-      for (let p; p < primesArr.length; p += 1) {
-        if (candidatePrime % p === 0) {
-          isPrime = false;
-          break;
-        }
-      }
-      if (isPrime) {
-        primesArr.push(candidatePrime)
-      }
-      return candidatePrime;
-    }
-    return null;
-  };
-
-  while (primeFactor !== targetNum) {
-    if (targetNum % primeFactor === 0) {
-      targetNum /= primeFactor;
-      factors.push(primeFactor);
-    } else {
-      primeFactor = newPrimeFactor(primeFactor);
-    }
+  // set up array of numbers
+  const nums = new Array(num - 2);
+  for (let i = 2; i <= num; i += 1) {
+    nums[indexConvert(i)] = i;
   }
-  console.log('factors', factors);
-  const prod = factors.reduce((c, a) => c * a);
-  console.log('prod', prod)
-  return primeFactor;
+
+  // find factors
+
+  for (let factor = 2; factor < num / 2; factor += 1) {
+    let numTimesUsed = 0;
+  }
+  return nums;
 };
 
 console.log(smallestMultiple(10));
