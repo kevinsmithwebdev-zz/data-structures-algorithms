2d6193035d4048d562e7a6b2cb6683ab94b1a77e
Kevin Smith <kevinsmithwebdev>
2019-01-27 11:00:19 -0600

    save

--- euler/src/005 Smallest Multiple/s1.js
+++ euler/src/005 Smallest Multiple/s1.js
@@ -1,19 +1,62 @@
-const smallestMultiple = (num) => {
-  const indexConvert = (num) => num - 2;
-  const factors = [];
+const makePrimes = max => {
+  console.log('max', max);
+  const sieve = new Array(max);
+  const maxCheck = max;
+  for (let i = 2; i <= maxCheck; i++) {
+    console.log('check1', i, sieve)
+    if (!sieve[i]) {
+      console.log(i, 'is prime')
+      for (let j = i * 2; j < max; j += i) {
+        console.log(j, 'is a multiple of', i)
+        sieve[j] = true
+      }
+    }
+  }
+  primes = [];
+  console.log('done sieve', sieve);
+  for (let i = 0; i < sieve.length; i++) {
+    if (!sieve[i]) {
+      console.log('i', i);
+      primes.push(i);
+    }
+  }
+  console.log('raw primes', primes);
+  primes.splice(0, 2);
+  return primes;
+}
 
+const smallestMultiple = (num) => { 
   // set up array of numbers
-  const nums = new Array(num - 2);
-  for (let i = 2; i <= num; i += 1) {
-    nums[indexConvert(i)] = i;
+  const numbers = new Array(num - 2);
+  for (let i = 2 ; i <= num; i += 1) {
+    numbers[i - 2] = i;
   }
+  const primes = makePrimes(Math.ceil(num/2));
+  console.log('primes', primes)
+  const factorsArr = [];
+  let count = 100
+  numbers.forEach(number => {
+    const factors = [];
+    if (!count--) return;
+    for (let i = 0; i < primes.length; i += 1) {
+      const factor = primes[i];
+      // console.log('factor =', factor);
+      let workingNum = number;
+      // console.log('testing', (workingNum > factor), (workingNum % factor === 0), 
+        // (workingNum > factor && (workingNum % factor === 0)));
+      while (workingNum > factor && (workingNum % factor === 0)) {
+        if (count-- < 0) return;
+        // console.log('workingNum = ', workingNum, factor);
+        factors.push(factor);
+        workingNum /= factor;
+      }
+    }
+    // console.log('factors', factors)
+    factorsArr.push(factors);
+  });
 
-  // find factors
-
-  for (let factor = 2; factor < num / 2; factor += 1) {
-    let numTimesUsed = 0;
-  }
-  return nums;
+  console.log('factorsArr', factorsArr)
+  return factorsArr;
 };
 
-console.log(smallestMultiple(10));
+smallestMultiple(10);
